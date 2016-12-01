6f99a6d3bc07672422c75a0338c47f7d38113849
Diego Dias <diegodias@diegodias.local>
2016-11-30 22:08:30 -0300

    fixed folder

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -41,8 +41,8 @@ export class PhotoComponent {
         console.log("new values for folder");
         let storage = firebase.storage();
         
-        this.fileList = this.af.database.list(`/${this.folder}/images`);
-        console.log("Rendering all images in ",`/${this.folder}/images`)
+        this.fileList = this.af.database.list(`/master`);
+        console.log("Rendering all images in ",`/master/`)
         this.imageList = this.fileList.map( itemList =>
             itemList.map( item => {
                 var pathReference = storage.ref(item.path);
@@ -56,7 +56,7 @@ export class PhotoComponent {
 
     upload() {
         // Create a root reference
-        let storageRef = firebase.storage().ref();
+        let storageRef = firebase.storage().ref('/master/');
 
         let success = false;
         // This currently only grabs item 0, TODO refactor it to grab them all
@@ -69,20 +69,20 @@ export class PhotoComponent {
             let path = `/${this.folder}/${selectedFile.name}`;
             var iRef = storageRef.child(path);
             iRef.put(selectedFile).then((snapshot) => {
-                console.log('Uploaded a blob or file! Now storing the reference at',`/${this.folder}/images/`);
-                af.database.list(`/${folder}/images/`).push({ path: path, filename: selectedFile.name })
+                console.log('Uploaded a blob or file! Now storing the reference at',`/master//`);
+                af.database.list(`/master`).push({ path: path, filename: selectedFile.name })
             });
         }
         
     }
     delete(image: Image) {
         let storagePath = image.path;
-        let referencePath = `${this.folder}/images/` + image.$key;
+        let referencePath = `/master/` + image.$key;
 
         // Do these as two separate steps so you can still try delete ref if file no longer exists
 
         // Delete from Storage
-        firebase.storage().ref().child(storagePath).delete()
+        firebase.storage().ref('/master/').child(storagePath).delete()
         .then(
             () => {},
             (error) => console.error("Error deleting stored file",storagePath)
